cd78c18b6477402a6bcd01b7dfcfdf41e81de176
AITuring <shspku365@gmail.com>
2021-04-10 02:11:32 +0800

    修改output为dist

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: '辽',
   favicon: 'http://image-aituring.test.upcdn.net/star.ico',
   logo: 'http://image-aituring.test.upcdn.net/star.ico',
-  outputPath: 'docs-dist',
+  outputPath: 'dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
 });
