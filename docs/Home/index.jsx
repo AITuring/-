c74e84212e7fbd992a0642bda79bb8b38327af70
AITuring <shspku365@gmail.com>
2021-04-13 23:51:45 +0800

    footer也不要了

--- docs/Home/index.jsx
+++ docs/Home/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { enquireScreen } from 'enquire-js';
-
-import Header from './Header';
 import Banner from './Banner';
 import Page1 from './Page1';
 import Page2 from './Page2';
-import Footer from './Footer';
 import './static/style';
 
 let isMobile;
@@ -30,13 +27,11 @@ class Home extends React.PureComponent {
     return (
       <DocumentTitle title="Ant Design - pro">
         <div>
-          {/* <Header isMobile={this.state.isMobile} /> */}
           <div className="home-wrapper">
             <Banner isMobile={this.state.isMobile} />
             <Page1 isMobile={this.state.isMobile} />
             <Page2 />
           </div>
-          <Footer />
         </div>
       </DocumentTitle>
     );
